5a4b0507607c9e2e99d7969410d8d685f4c73cda
Ed Scheinerman <ers@jhu.edu>
2016-06-18 14:26:06 -0400

    Building IntervalOrder

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Simple Poset Representations
 
 Interval orders, and the like.
+
+## Interval orders
+
+An *interval order* is a partially ordered set whose vertices
+correspond to closed intervals. We have the relation `a<b` exactly when
+the interval assigned to `a` is entirely to the left of the interval
+assigned to `b`.
+
+The `IntervalOrder` function creates a new interval order given
+a collection of closed intervals in one of the following ways:
++ As a dictionary mapping element names to intervals.
++ As a list (one-dimensional array) of intervals. In this case,
+the elements are named `1:n` where `n` is the length of the list.
++ As a set of intervals. In this case the intervals themselves
+are the elements.
+
+## Semiorders
+
+## Circle orders
--- src/SimplePosetRepresentations.jl
+++ src/SimplePosetRepresentations.jl
@@ -4,5 +4,61 @@ module SimplePosetRepresentations
 
 using SimplePosets, ClosedIntervals
 
+export IntervalOrder
+
+"""
+`IntervalOrder(Jmap)` creates an inteval order from a dictionary
+mapping elements to closed intervals.
+"""
+function IntervalOrder{S,T}(Jmap::Dict{S,ClosedInterval{T}})
+  P = SimplePoset{S}()
+  for v in keys(Jmap)
+    add!(P,v)
+  end
+  verts = elements(P)
+  n = length(verts)
+  for i=1:n-1
+    u = verts[i]
+    I = Jmap[u]
+    for j=i+1:n
+      v = verts[j]
+      J = Jmap[v]
+      if I << J
+        add!(P,u,v)
+      end
+    end
+  end
+  return P
+end
+
+
+"""
+`IntervalOrder(Jlist)` creates an interval order from a list of
+intervals. The elements of the poset are named `1:n` where `n`
+is the length of the `Jlist`.
+"""
+function IntervalOrder{T}(Jlist::Vector{ClosedInterval{T}})
+  d = Dict{Int,ClosedInterval{T}}()
+  n = length(Jlist)
+  for k=1:n
+    d[k] = Jlist[k]
+  end
+  return IntervalOrder(d)
+end
+
+
+"""
+`IntervalOrder(Jset)` creates an interval order from a set of
+closed intervals. The elements of the poset are the intervals.
+"""
+function IntervalOrder{T}(Jset::Set{ClosedInteral{T}})
+  Jtype = ClosedInterval{T}
+  d = Dict{Jtype,Jtype}()
+  for J in Jset
+    d[J] = J
+  end
+  return IntervalOrder(d)
+end
+
 
 end  # end of module
